b29a6a8841965f8a12bd05ec00205bf7012b3179
yejin-won <100192779+yejin-won@users.noreply.github.com>
2022-04-06 16:43:47 +0900

    commit

--- Funding/src/main/java/funding/command/MainCommand.java
+++ Funding/src/main/java/funding/command/MainCommand.java
@@ -5,24 +5,15 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-<<<<<<< HEAD
 import javax.servlet.http.HttpSession;
-=======
->>>>>>> origin/yimoonyeol
 
 public class MainCommand implements FCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
 
 		HttpSession session = request.getSession();
 		session.getAttribute("customer_id");
-		
-=======
-		// TODO Auto-generated method stub
-
->>>>>>> origin/yimoonyeol
 	}
 
 }
--- Funding/src/main/java/funding/command/SSignUpCommand.java
+++ Funding/src/main/java/funding/command/SSignUpCommand.java
@@ -14,17 +14,20 @@ public class SSignUpCommand implements FCommand {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String seller_id = request.getParameter("seller_id");
-		String seller_pw = request.getParameter("seller_pw");
-		String seller_licence =request.getParameter("seller_licence");
-		String seller_number =request.getParameter("seller_number");
+		String seller_pw = request.getParameter("seller_pw1");
+		String seller_pw2 =request.getParameter("seller_pw2");
+		String seller_number =request.getParameter("seller_number1") + "-" + request.getParameter("seller_number2")
+								+"-" + request.getParameter("seller_number3");
 		String seller_profile = request.getParameter("seller_profile");
 		String seller_name = request.getParameter("seller_name");
-		String seller_phone = request.getParameter("seller_phone");
+		String seller_phone = request.getParameter("seller_phone1") + "-" + request.getParameter("seller_phone2")
+								+"-" + request.getParameter("seller_phone3");
 		String seller_person_name = request.getParameter("seller_person_name");
-		String seller_person_phone = request.getParameter("seller_person_phone");
+		String seller_person_phone = request.getParameter("seller_person_phone1")+"-"+request.getParameter("seller_person_phone2")
+								+"-"+request.getParameter("seller_person_phone3");
 		
 		FDaoS daoS = new FDaoS();
-		daoS.ssignUp(seller_id, seller_pw, seller_licence, seller_number, seller_profile, seller_name, seller_phone, 
+		daoS.ssignUp(seller_id, seller_pw, seller_pw2, seller_number, seller_profile, seller_name, seller_phone, 
 				seller_person_name, seller_person_phone);
 	}
 
