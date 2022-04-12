693dbae570d61ddbc089838b5795b52f97eea4ce
dongwon <davidshin1213@gmail.com>
2022-04-12 16:55:29 +0900

    날아가지마라22

--- Funding/src/main/java/funding/command/FundingListViewCommand.java
+++ Funding/src/main/java/funding/command/FundingListViewCommand.java
@@ -25,15 +25,7 @@ public class FundingListViewCommand implements FCommand {
 		ArrayList<FDtoFunding> list = dao.fundinglistview(page);
 		
 		request.setAttribute("funding", list);
-<<<<<<< HEAD
 		request.setAttribute("countQuestion", countFunding);
-=======
-<<<<<<< HEAD
-
-=======
-		request.setAttribute("countQuestion", countFunding);
->>>>>>> newdong
->>>>>>> main
 	}
 
 }
